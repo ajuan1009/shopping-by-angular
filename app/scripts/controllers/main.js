9f0a9b3b6e4eb2f90e69754dfa04750441b4a228
unknown <756477013@qq.com>
2016-11-23 11:18:46 +0800

    init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shoppingByAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shoppingByAngularApp
+ */
+angular.module('shoppingByAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
